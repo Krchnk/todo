49997afe94391f10d65cec1d83287a1d72c11872
Krchnk <a4mesh@gmail.com>
2025-02-16 23:52:22 +0300

    init

---
+++ internal/service/service.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"todo"
+	"todo/internal/repository"
+)
+
+type Authorization interface {
+	CreateUser(user todo.User) (int, error)
+}
+type TodoList interface {
+}
+type TodoItem interface {
+}
+
+type Service struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewService(repos *repository.Repository) *Service {
+	return &Service{Authorization: NewAuthService(repos.Authorization)}
+}
